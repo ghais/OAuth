381b59fb14f7397a5f06757bb08bb8f76bea47da
ghais <ghais.issa@gmail.com>
2013-10-27 15:01:54 -0400

    Create the app in a new window
    This way it is easier to inspect the request/response in the debug tools.

--- background.js
+++ background.js
@@ -4,21 +4,5 @@
  * @see http://developer.chrome.com/trunk/apps/app.runtime.html
  * @see http://developer.chrome.com/trunk/apps/app.window.html
  */
-//window.open('index.html');
+window.open('index.html');
 
-chrome.app.runtime.onLaunched.addListener(function () {
-   //Center window on screen.
-  var screenWidth = screen.availWidth;
-  var screenHeight = screen.availHeight;
-  var width = screenWidth/2;
-  var height = screenHeight/2;
-
-  chrome.app.window.create('index.html', {
-    bounds: {
-      width: width,
-      height: height,
-      left: Math.round((screenWidth-width)/2),
-      top: Math.round((screenHeight-height)/2)
-    }
-  });
-});
